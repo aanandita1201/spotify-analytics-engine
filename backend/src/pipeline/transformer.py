18f7333806f1c4d3e64fba5facf074ce0a7d5a67
Aanandita <175534526+aanandita1201@users.noreply.github.com>
2026-08-22 23:52:11 +0530

    Add user timezone to timestamp transformations

--- backend/src/pipeline/transformer.py
+++ backend/src/pipeline/transformer.py
@@ -1,16 +1,17 @@
 import pandas as pd
 
 
-def transform_streaming_history(df):
+def transform_streaming_history(df, timezone):
     df = df.copy()
 
-    df["date"] = df["ts"].dt.date
-    df["year"] = df["ts"].dt.year
-    df["month"] = df["ts"].dt.month
-    df["month_name"] = df["ts"].dt.month_name()
-    df["day_of_week"] = df["ts"].dt.day_name()
-    df["hour"] = df["ts"].dt.hour
+    df["local_ts"] = df["ts"].dt.tz_convert(timezone)
 
+    df["date"] = df["local_ts"].dt.date
+    df["year"] = df["local_ts"].dt.year
+    df["month"] = df["local_ts"].dt.month
+    df["month_name"] = df["local_ts"].dt.month_name()
+    df["day_of_week"] = df["local_ts"].dt.day_name()
+    df["hour"] = df["local_ts"].dt.hour
     df["minutes_played"] = df["ms_played"] / 60000
 
     return df
